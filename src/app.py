f303e0366c7aa6fe431a9b300ab58b29509507a9
Psangwon <pktony@naver.com>
2025-05-04 22:48:53 +0900

    초기모델 v1 학습

--- src/app.py
+++ src/app.py
@@ -1,6 +1,16 @@
 from ultralytics import YOLO
 
-model = YOLO('yolov8n.pt')
-results = model("https://ultralytics.com/images/bus.jpg", save=True)
+model = YOLO('yolov8n.pt', verbose=True)
+# results = model("https://ultralytics.com/images/bus.jpg", save=True)
 
-results[0].save(filename="result.jpg")
+# results[0].save(filename="result.jpg")
+
+try :
+    model.train(
+        data="/Users/psangwon/Documents/GitRepository/python/soccer-detector/dataset/data.yaml",
+        epochs=100,
+        batch=16,
+        imgsz=640
+    )
+except Exception as e :
+    print(e)
