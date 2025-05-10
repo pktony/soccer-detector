2c1c83e51445ef042fd197d4623c2873fa05ee61
psangwon <pktony2011@gmail.com>
2025-05-10 18:01:17 +0900

    [soccernet] tracking

--- src/soccernet/index_2.py
+++ src/soccernet/index_2.py
@@ -1,4 +1,9 @@
 from downloader import download_tracking
-
+from unzip import unzip_soccerNet
+from parse_labels import parse_labels_recursive
 
 download_tracking('./dataset/SoccerNet_Tracking')
+unzip_soccerNet('./dataset/SoccerNet_Tracking')
+parse_labels_recursive(
+    local_path='./dataset/SoccerNet_Tracking',
+    output_path = './dataset/SoccerNet_Tracking/yolo_dataset')
