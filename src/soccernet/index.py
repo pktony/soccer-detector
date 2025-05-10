30a61111b97416542506a12b64217bb65581b104
psangwon <pktony2011@gmail.com>
2025-05-10 12:54:36 +0900

    [soccernet] add donwnload

--- src/soccernet/index.py
+++ src/soccernet/index.py
@@ -1,7 +1,9 @@
 from unzip import unzip_soccerNet
 from parse_labels import parse_labels_recursive
+from downloader import download_soccerNet
 
 
+download_soccerNet('./dataset/SoccerNet')
 unzip_soccerNet('./dataset/SoccerNet')
 parse_labels_recursive(
     local_path='./dataset/SoccerNet',
