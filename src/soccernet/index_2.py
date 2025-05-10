e0ac7f5f1fd33c18a894485c617e912fce7f219a
psangwon <pktony2011@gmail.com>
2025-05-10 18:44:01 +0900

    [soccernet] try except

--- src/soccernet/index_2.py
+++ src/soccernet/index_2.py
@@ -2,8 +2,11 @@ from downloader import download_tracking
 from unzip import unzip_soccerNet
 from parse_labels import parse_labels_recursive
 
-download_tracking('./dataset/SoccerNet_Tracking')
-unzip_soccerNet('./dataset/SoccerNet_Tracking')
-parse_labels_recursive(
-    local_path='./dataset/SoccerNet_Tracking',
-    output_path = './dataset/SoccerNet_Tracking/yolo_dataset')
+try :
+    download_tracking('./dataset/SoccerNet_Tracking')
+    unzip_soccerNet('./dataset/SoccerNet_Tracking')
+    parse_labels_recursive(
+        local_path='./dataset/SoccerNet_Tracking',
+        output_path = './dataset/SoccerNet_Tracking/yolo_dataset')
+except Exception as e :
+    print(e)
