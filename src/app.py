0a30f0dd7cc06371c260b22e1dfafaf9622e2a45
psangwon <pktony2011@gmail.com>
2025-05-24 03:09:54 +0900

    [kmeans] add clustering

--- src/app.py
+++ src/app.py
@@ -1,4 +1,5 @@
 from ultralytics import YOLO
+import cv2
 
 model = YOLO('yolov8n.pt', verbose=True)
 # results = model("https://ultralytics.com/images/bus.jpg", save=True)
@@ -22,8 +23,16 @@ from tracker import Tracker
 
 video_frames = read_video('./08fd33_4.mp4')
 
-tracker = Tracker(model_path='./train/train6/weights/best.pt')
+tracker = Tracker(model_path='./train/train_results_v6/train6/weights/best.pt')
 tracks = tracker.get_object_tracks(video_frames, read_from_stub = True, stub_path = 'stubs/cam_move.pkl')
+
+for track_id, player in tracks['player'][0].items():
+    bbox = player['bbox']
+    frame = video_frames[0]
+
+    cropped_image = frame[int(bbox[1]):int(bbox[3]), int(bbox[0]):int(bbox[2])] # [bbox[1]:bbox[3], bbox[0]:bbox[2]]
+    cv2.imwrite(f'.cropped_image.jpg', cropped_image)
+    break
 
 output_video_frames = tracker.draw_annotations(video_frames, tracks)
 
--- src/tracker/tracker.py
+++ src/tracker/tracker.py
@@ -17,7 +17,7 @@ class Tracker:
         batch_size = 20
         detections = []
         for i in range(0, len(frames), batch_size):
-            detections_batch = self.model.predict(frames[i:i+batch_size], conf = 0.1, verbose=False)
+            detections_batch = self.model.predict(frames[i:i+batch_size], conf = 0.2, verbose=False)
             detections += detections_batch
         
         return detections
